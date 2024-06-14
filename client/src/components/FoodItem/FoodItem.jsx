fd231703dea5cdb76d23d1812b1c706a46befffb
Rijo Sebastian <rijobdk@gmail.com>
2024-06-14 17:15:40 +0530

    Refactor code to use context api to manage state -  Create a context file to manage state

--- client/src/components/FoodItem/FoodItem.jsx
+++ client/src/components/FoodItem/FoodItem.jsx
@@ -1,17 +1,18 @@
-import { useState } from "react";
+import { useContext } from "react";
 import { assets } from "../../assets/assets";
+import { StoreContext } from "../../context/StoreContext";
 
-const FoodItem = ({ name, price, description, image }) => {
-  const [itemCount, setItemCount] = useState(0);
+const FoodItem = ({ _id: id, name, price, description, image }) => {
+  const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
   return (
     <div className="food-item w-full m-auto rounded-2xl shadow animate-fadeIn">
       <div className="food-item-img-container relative ">
         <img src={image} alt={name} className="rounded-t-xl w-full" />
-        {!itemCount ? (
+        {!cartItems[id] ? (
           <img
             src={assets.add_icon_white}
             alt="add"
-            onClick={() => setItemCount((prev) => prev + 1)}
+            onClick={() => addToCart(id)}
             className="absolute bottom-3.5 right-3.5  w-8 rounded-full cursor-pointer"
           />
         ) : (
@@ -19,14 +20,14 @@ const FoodItem = ({ name, price, description, image }) => {
             <img
               src={assets.remove_icon_red}
               alt="remove icon"
-              onClick={() => setItemCount((prev) => prev - 1)}
+              onClick={() => removeFromCart(id)}
               className="w-8 cursor-pointer"
             />
-            <p>{itemCount}</p>
+            <p>{cartItems[id]}</p>
             <img
               src={assets.add_icon_green}
               alt="add icon"
-              onClick={() => setItemCount((prev) => prev + 1)}
+              onClick={() => addToCart(id)}
               className="w-8 cursor-pointer"
             />
           </div>
