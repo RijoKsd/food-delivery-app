101bf9e301d166daea3eb575ae0b08194823c813
Rijo Sebastian <rijobdk@gmail.com>
2024-06-14 17:35:43 +0530

    Add filter the menu option to display the menu items based on the category selected

--- client/src/components/FoodDisplay/FoodDisplay.jsx
+++ client/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,20 +1,22 @@
-import { useContext } from "react"
-import { StoreContext } from "../../context/StoreContext"
+import { useContext } from "react";
+import { StoreContext } from "../../context/StoreContext";
 import FoodItem from "../FoodItem/FoodItem";
 
- 
-const FoodDisplay = ({category}) => {
-    const {food_list} = useContext(StoreContext)
-   return (
-     <div id="food-display" className="food-display mt-8">
-       <h2 className="text-2xl font-semibold">Top dishes near you</h2>
-       <div className="food-display-list grid mt-4 p-8 gap-5 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-         {food_list.map((item, index) => (
-           <FoodItem key={index} {...item} />
-         ))}
-       </div>
-     </div>
-   );
-}
+const FoodDisplay = ({ category }) => {
+  const { food_list } = useContext(StoreContext);
+  return (
+    <div id="food-display" className="food-display mt-8">
+      <h2 className="text-2xl font-semibold">Top dishes near you</h2>
+      <div className="food-display-list grid mt-4 p-8 gap-5 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {food_list.map((item, index) => {
+          if (category === "all" || category === item.category) {
+            console.log(item, "item");
+            return <FoodItem key={index} {...item} />;
+          }
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default FoodDisplay
+export default FoodDisplay;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -16,7 +16,7 @@ export default {
         },
       },
       animation: {
-        fadeIn: "fadeIn 2s ease-in-out",
+        fadeIn: "fadeIn 1s ease-in-out",
       }
     },
   },
