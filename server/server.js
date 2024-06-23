a4018aed5a8e71f5143573f831ee090853cbdf7f
Rijo Sebastian <rijobdk@gmail.com>
2024-06-23 21:46:34 +0530

    Add routes to user registration and login - Add userController.js - Add userModel.js - Add userRoute.js - Modify server.js to include userRoute.js - Add JWT token to user registration and login

--- server/server.js
+++ server/server.js
@@ -1,28 +1,27 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-
 import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+
 
 // app config
-
 dotenv.config();
 const app = express();
-
 const port = process.env.PORT || 4000;
 
 // middleware
-
 app.use(express.json());
 app.use(cors());
 
-// db connection
+// mongodb connection
 connectDB();
 
 // api endpoints
 app.use("/api/food", foodRouter);
 app.use("/images", express.static("uploads"));
+app.use("/api/user", userRouter)
 
 app.get("/", (req, res) => {
   res.send("Hello World");
