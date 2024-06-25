2854b7750afce36652f385876bf0a1c704c53a3f
Rijo Sebastian <rijobdk@gmail.com>
2024-06-25 22:20:35 +0530

    Update config.js

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,5 +1,5 @@
 const config = {
-  apiUrl: "https://food-delivery-app-backend-81ac.onrender.com/",
+  apiUrl: "https://food-delivery-app-backend-81ac.onrender.com",
   // Add other configuration settings here
   
 };
